ae17ffdb37f8d5c3e92d9c07a2d57348925f2460
Nikola <nikola88393@gmail.com>
2023-08-12 16:52:49 +0300

    Add player choice and play a round method

--- script.js
+++ script.js
@@ -2,20 +2,51 @@ function getComputerChoice(){
     let number = Math.floor(Math.random()*(3)+1);
     switch(number){
         case 1:
-            return "Rock";
-            break;
+            return "rock";
         case 2:
-            return "Paper";
-            break;
+            return "paper";
         case 3:
-            return "Scissors";
-            break;
+            return "scissors";
         default:
-            return "Error";
-            break;
+            return "error";
+    }
+
+}
+
+function getPlayerSelection(){
+    let input = prompt("Choose etiher rock, paper or scissors:")
+    let convertedInput = input.toLowerCase();
+    if(convertedInput!= "rock" && convertedInput != "paper" && convertedInput != "scissors"){
+        alert("Invalid input!");
+    }
+    else {
+        return convertedInput;
+    }
+    console.log(convertedInput);
+}
+
+function playSingleRound(computerSelection, playerSelection){
+    if (computerSelection === playerSelection){
+        console.log("it's a tie!");
+    }
+    else if(computerSelection === "rock" && playerSelection === "scissors"){
+        console.log("Computer wins this round!");
+    }
+    else if(computerSelection === "rock" && playerSelection === "paper"){
+        console.log("Player wins this round!");
+    }
+    else if(computerSelection === "scissors" && playerSelection === "paper"){
+        console.log("Computer wins this round!");
+    }
+    else if(computerSelection === "scissors" && playerSelection === "rock"){
+        console.log("Player wins this round!");
+    }
+    else if(computerSelection === "paper" && playerSelection === "rock"){
+        console.log("Computer wins this round!");
+    }
+    else if(computerSelection === "paper" && playerSelection === "scissors"){
+        console.log("Player wins this round!");
     }
 }
 
-for(let i = 0; i < 10; i++){
-    console.log(getComputerChoice());
-}
+console.log(playSingleRound(getComputerChoice(), getPlayerSelection()));
