c7ddd28b52e63704f427d446800645fcf273c7e5
Nikola <nikola88393@gmail.com>
2023-08-12 17:08:29 +0300

    Add game function. Game is now playable

--- script.js
+++ script.js
@@ -26,27 +26,71 @@ function getPlayerSelection(){
 }
 
 function playSingleRound(computerSelection, playerSelection){
+    let winner = "";
     if (computerSelection === playerSelection){
         console.log("it's a tie!");
+        alert("it's a tie!");
+        winner = "tie";
     }
     else if(computerSelection === "rock" && playerSelection === "scissors"){
         console.log("Computer wins this round!");
+        alert("Computer wins this round!");
+        winner = "computer";
     }
     else if(computerSelection === "rock" && playerSelection === "paper"){
         console.log("Player wins this round!");
+        alert("Player wins this round!");
+        winner = "player";
     }
     else if(computerSelection === "scissors" && playerSelection === "paper"){
         console.log("Computer wins this round!");
+        alert("Computer wins this round!");
+        winner = "computer";
     }
     else if(computerSelection === "scissors" && playerSelection === "rock"){
         console.log("Player wins this round!");
+        alert("Player wins this round!");
+        winner = "player";
     }
     else if(computerSelection === "paper" && playerSelection === "rock"){
         console.log("Computer wins this round!");
+        alert("Computer wins this round!");
+        winner = "computer";
     }
     else if(computerSelection === "paper" && playerSelection === "scissors"){
         console.log("Player wins this round!");
+        alert("Player wins this round!");
+        winner = "player";
     }
+
+    return winner;
 }
 
-console.log(playSingleRound(getComputerChoice(), getPlayerSelection()));
+function game(){
+    let round = 0, scorePlayer = 0, scoreComputer = 0;
+    while (round <= 5){
+        let winner = playSingleRound(getComputerChoice(), getPlayerSelection());
+        if(winner === "tie"){
+            scorePlayer++;
+            scoreComputer++;
+        }
+        else if(winner === "player"){
+            scorePlayer++;
+        }
+        else if(winner === "computer"){
+            scoreComputer++;
+        }
+        round++;
+    }
+    alert(`Player: ${scorePlayer}; Computer: ${scoreComputer}`);
+    if(scoreComputer > scorePlayer){
+        alert("Computer wins!");
+    }
+    else {
+        alert("Player wins!");
+    }
+    
+}
+
+console.log(game());
+
